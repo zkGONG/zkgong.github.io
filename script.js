e0bfc05259916e2d17e43a8e9ca73b0d7aa646f8
zkGONG <139263474+zkGONG@users.noreply.github.com>
2024-08-13 10:48:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// 你可以在这里添加一些交互效果，例如平滑滚动
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
